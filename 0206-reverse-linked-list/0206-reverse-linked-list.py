a1dd0920dee20cde0a5f4b14656b71ec24fd2c82
Binayak Basu <121999431+basu-binayak@users.noreply.github.com>
2023-08-02 18:15:06 +0530

    Time: 38 ms (97.56%), Space: 17.9 MB (43.77%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # The provided code defines a Python class Solution with a method reverseList. The method
+        # takes a singly-linked list represented by the head node and returns the reversed version of
+        # the linked list.
+        
+        # The idea is to use three pointers curr, prev, and next to keep track of nodes to update
+        # reverse links.
+        
+        # Follow the steps below to solve the problem:
+
+        # Initialize three pointers prev as NULL, curr as head, and next as NULL.
+        prev , curr, next = None, head , None 
+        
+        # Iterate through the linked list. In a loop, do the following:
+        while curr!=None:
+            
+            # Before changing the next of curr, store the next node
+            # next = curr -> next
+            next = curr.next 
+            
+            # Now update the next pointer of curr to the prev
+            # curr -> next = prev
+            curr.next = prev
+            
+            # Update prev as curr and curr as next 
+            prev = curr
+            curr = next
+        
+        return prev
+            
+        
+        
