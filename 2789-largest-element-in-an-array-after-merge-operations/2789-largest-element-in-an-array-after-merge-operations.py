2c7e0e860c412d1a89119c77f0e862da319ac4ba
Binayak Basu <121999431+basu-binayak@users.noreply.github.com>
2023-08-02 15:40:27 +0530

    Time: 868 ms (92.36%), Space: 27.2 MB (89.86%) - LeetHub

--- 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.py
+++ 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.py
@@ -1,11 +1,24 @@
 class Solution:
     def maxArrayValue(self, nums: List[int]) -> int:
+        # the approach to this problem is simple: instead of going from left
+        # to right through nums list , move from right to left. The reason
+        # for this is the condition that we are checking! 
+        # if nums[i]<=nums[i+1] , replace nums[i+1] by nums[i]+nums[i+1] and 
+        # delete nums[i]
         index = len(nums)-1
+        
+        # check if there exists an index-1 position in list 
         while index-1>=0:
             if nums[index-1]<=nums[index]:
                 nums[index] = nums[index-1] + nums[index]
                 nums.pop(index-1)
-                index-=1
+                # note
+                # consider [2,3,7,9,3]
+                # after one iteration in while loop, the nums= [2,3,16,3]
+                # index equals 3. We want the index to point at the newly 
+                # formed elelemnt i.e. 16. So must decrease the index by 1
+                # since one element is deleted. 
+                index-=1   
             else:
                 index-=1
         return max(nums)
