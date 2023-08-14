d77ea5de8cc09beece0fe711da8634b545c42257
Binayak Basu <121999431+basu-binayak@users.noreply.github.com>
2023-08-14 09:29:31 +0530

    Added solution - LeetHub

---
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    def kthElement(self,  arr1, arr2, n, m, k):
+        # note k starts from 1 i.e. the first element is at pos 1 
+        if n > m:
+            return self.kthElement(arr2, arr1, m, n, k)
+        if n == 0:
+            return arr2[k-1]
+        if k == 1:
+            return min(arr1[0], arr2[0])
+        i = min(n, k//2)
+        j = min(m, k//2)
+        if arr1[i-1] > arr2[j-1]:
+            return self.kthElement(arr1, arr2[j:], n, m-j, k-j)
+        else:
+            return self.kthElement(arr1[i:], arr2, n-i, m, k-i)
+            
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, m, k = sz[0], sz[1], sz[2]
+        a = [int(x) for x in input().strip().split()]
+        b = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.kthElement( a, b, n, m, k))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
