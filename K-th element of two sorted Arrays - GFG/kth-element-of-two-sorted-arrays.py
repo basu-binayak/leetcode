df17d4c44df5a2d420237bba5034f50f0cd557ed
Binayak Basu <121999431+basu-binayak@users.noreply.github.com>
2023-08-14 10:03:51 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.py
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.py
@@ -3,19 +3,25 @@
 class Solution:
     def kthElement(self,  arr1, arr2, n, m, k):
         # note k starts from 1 i.e. the first element is at pos 1 
-        if n > m:
+        # n = len(arr1)
+        # m = len(arr2)
+        # k = the kth element position in the final sorted array
+        if n > m:  # to ensure arr1 is always the smaller array
             return self.kthElement(arr2, arr1, m, n, k)
-        if n == 0:
-            return arr2[k-1]
-        if k == 1:
+        if n == 0:  # if arr1 is empty, then kth element is at index k-1 in arr2
+            return arr2[k-1]  # kth element is at index k-1 in arr2
+        if k == 1:  # if k is 1, then the smallest element is the minimum of the first elements of both arrays
             return min(arr1[0], arr2[0])
-        i = min(n, k//2)
-        j = min(m, k//2)
-        if arr1[i-1] > arr2[j-1]:
+        i = min(n, k//2)  # to ensure we're not overshooting the kth position
+        j = min(m, k//2)  # to ensure we're not overshooting the kth position
+    
+        if arr1[i-1] > arr2[j-1]:  # if arr1[i-1] is greater, it means we can discard the first j elements of arr2 because those elements are guaranteed to be smaller than the kth element
+    
+            # recursively call the function with a modified set of parameters where arr2 starts from index j and k is reduced by j
             return self.kthElement(arr1, arr2[j:], n, m-j, k-j)
         else:
+            # recursively call the function with a modified set of parameters where arr1 starts from index i and k is reduced by i
             return self.kthElement(arr1[i:], arr2, n-i, m, k-i)
-            
         
 
 
